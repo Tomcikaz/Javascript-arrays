e1070ecbe96fe7fb53aa20a4c1bcdf514f4b0002
Tomas <Tomcikaz@yahoo.com>
2020-10-17 15:47:21 +0000

    add search-list.js

--- search-list.js
+++ search-list.js
@@ -1,3 +1,11 @@
 const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
 const search = prompt('Search for a product.');
 let message;
+
+if (inStock.includes(search.toLowerCase() ) ) {
+message = `We have <strong> ${search} </strong> in stock!`; 
+}
+else {
+ message = `Sorry we don't have any <strong> ${search} </strong> :(`; 
+}
+document.querySelector("main").innerHTML = `<p>${message}</p>`;
