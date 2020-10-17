61a588b591ac79891c48ae73473bc4db45e14282
Tomas <Tomcikaz@yahoo.com>
2020-10-17 16:03:07 +0000

    addet toLowerCase() function

--- search-list.js
+++ search-list.js
@@ -3,9 +3,9 @@ const search = prompt('Search for a product.');
 let message;
 
 if (inStock.includes(search.toLowerCase() ) ) {
-message = `We have <strong> ${search} </strong> in stock!`; 
+message = `We have <strong> ${search.toLowerCase()} </strong> in stock!`; 
 }
 else {
- message = `Sorry we don't have any <strong> ${search} </strong> :(`; 
+ message = `Sorry we don't have any <strong> ${search.toLowerCase()} </strong> :(`; 
 }
 document.querySelector("main").innerHTML = `<p>${message}</p>`;
